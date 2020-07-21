fc2cf2d6a79f1e4f1d9df13cbb33f2ea1072f099
sajozsattila <sajozsattila@disroot.org>
2020-07-21 15:26:06 +0000

    work on prrof relationship

--- app/static/js/mur2.js
+++ app/static/js/mur2.js
@@ -51,11 +51,8 @@ function clear_cookies(cname) {
 
 // collapsible divs
 function collapsible_listener() {
-    console.log(document);
     var coll = document.getElementsByClassName("collapsible");
-    var i;
-    console.log(coll.length);
-    for (i = coll.length; i--;) {
+    for (var i = coll.length; i--;) {
         
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
